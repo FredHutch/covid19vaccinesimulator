62a1239434b644863a2417bc6985a270032893f1
Pei-Yao Hung <peiyaoh@gmail.com>
2023-04-18 16:32:44 -0400

    Update dev-history-info.js

--- src/content/dev-history-info.js
+++ src/content/dev-history-info.js
@@ -1,9 +1,9 @@
 
 
 export var devHistoryInfoList = [
-    {status: 'Second version released and published online.', date: '10/15/2023', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
-    {status: 'First version released and published online.', date: '03/01/2023', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
     
+    {status: 'First version released and published online.', date: '04/18/2023', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
+    {status: 'Initiated the design and development process', date: '04/01/2022', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
     /*
     {status: 'Processing', date: '15/10/2020', icon: 'pi pi-cog', color: '#673AB7'},
     {status: 'Shipped', date: '15/10/2020', icon: 'pi pi-shopping-cart', color: '#FF9800'},
