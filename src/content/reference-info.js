02d2606e1c02ce74e7848dee88865f3e2eed1dc5
Pei-Yao Hung <peiyaoh@gmail.com>
2024-01-29 00:24:47 -0500

    add PLOS paper

--- src/content/reference-info.js
+++ src/content/reference-info.js
@@ -2,11 +2,41 @@
 
 export var referenceInfoList = [
     {
+        label: "Trejo I., Hung P-Y., Matrajt L. Covid19Vaxplorer: A free, online, user-friendly COVID-19 vaccine allocation comparison tool. PLOS Glob Public Health 4(1): e0002136.",
+        year: 2024,
+        links: [
+            {
+                label: "Online",
+                link: "https://doi.org/10.1371/journal.pgph.0002136"
+            }
+        ]
+    },
+    {
+        label: "Matrajt L., Brown E.R., Dimitrov D. and Janes H. The role of antiviral treatment in curbing the COVID-19 pandemic: a modeling study Matrajt, L., Brown, E.R., Cohen, M.S. et al. Could widespread use of antivi- ral treatment curb the COVID-19 pandemic? A modeling study. BMC Infectious Diseases 22, 683.",
+        year: 2022,
+        links: [
+            {
+                label: "Online",
+                link: "https://doi.org/10.1186/s12879-022-07639-1"
+            }
+        ]
+    },
+    {
+        label: "Bracis C., Moore M., Swan D.A., Matrajt L. Improving vaccination coverage and offering vaccine to all school-age children will allow uninterrupted in-person schooling in King County, WA: Modeling analysis, Mathematical Biosciences and Engineering, Volume 19, Issue 6: 5699-5716.",
+        year: 2022,
+        links: [
+            {
+                label: "Online",
+                link: "http://www.aimspress.com/article/doi/10.3934/mbe.2022266"
+            }
+        ]
+    },
+    {
         label: "Matrajt, L., Eaton, J., Leung, T., & Brown, E.R. Vaccine optimization for COVID-19: Who to vaccinate first?. Science Advances, 7(6)",
         year: 2021,
         links: [
             {
-                label:"Online",
+                label: "Online",
                 link: "https://doi.org/10.1126/sciadv.abf1374"
             }
         ]
@@ -16,58 +46,38 @@ export var referenceInfoList = [
         year: 2021,
         links: [
             {
-                label:"Online",
+                label: "Online",
                 link: "https://doi.org/10.1038/s41467-021-23761-1"
             }
         ]
     },
-     {
+    {
         label: "Matrajt L., Janes H., Schiffer J.T. and Dimitrov D. Quantifying the impact of lifting community non-pharmaceutical interventions for COVID-19 during vaccination rollout in the United States. Open Forum Infectious Diseases, Vol. 8, Issue 7",
         year: 2021,
         links: [
             {
-                label:"Online",
+                label: "Online",
                 link: "https://doi.org/10.1093/ofid/ofab341"
             }
         ]
     },
+
     {
-        label: "Matrajt L., Brown E.R., Dimitrov D. and Janes H. The role of antiviral treatment in curbing the COVID-19 pandemic: a modeling study Matrajt, L., Brown, E.R., Cohen, M.S. et al. Could widespread use of antivi- ral treatment curb the COVID-19 pandemic? A modeling study. BMC Infectious Diseases 22, 683.",
-        year: 2022,
-        links: [
-            {
-                label:"Online",
-                link: "https://doi.org/10.1186/s12879-022-07639-1"
-            }
-        ]
-    },
-        {
         label: "Swan D. A., Bracis C., Janes H., Moore M., Matrajt L., et al. COVID-19 vaccines that reduce symptoms but do not block infection need higher coverage and faster rollout to achieve population impact. Scientific Reports",
         year: 2021,
         links: [
             {
-                label:"Online",
+                label: "Online",
                 link: "https://doi.org/10.1038/s41598-021-94719-y"
             }
         ]
     },
     {
-        label: "Bracis C., Moore M., Swan D.A., Matrajt L. Improving vaccination coverage and offering vaccine to all school-age children will allow uninterrupted in-person schooling in King County, WA: Modeling analysis, Mathematical Biosciences and Engineering, Volume 19, Issue 6: 5699-5716.",
-        year: 2022,
-        links: [
-            {
-                label:"Online",
-                link: "http://www.aimspress.com/article/doi/10.3934/mbe.2022266"
-            }
-        ]
-    },
-  
-        {
         label: "Matrajt L and Leung T. Evaluating the Effectiveness of Social Distancing Interventions to Delay or Flatten the Epidemic Curve of Coronavirus Disease. Emerging Infectious Diseases, 26(8):1740-1748",
-        year:2020 ,
+        year: 2020,
         links: [
             {
-                label:"Online",
+                label: "Online",
                 link: "https://doi.org/10.3201/eid2608.201093"
             }
         ]
